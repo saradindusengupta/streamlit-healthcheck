644873dd8bfe0985d80ab51c683928890eb98dc9
Saradindu Sengupta <saradindu.mi1@iiitmk.ac.in>
2025-04-05 15:27:27 +0530

    Add author, email, license, and copyright information to info.py

--- src/streamlit_healthcheck/info.py
+++ src/streamlit_healthcheck/info.py
@@ -1,2 +1,5 @@
 __version__ = "0.0.1"
 __author__ = "Saradindu Sengupta"
+__email__="saradindu.mi1@iiitmk.ac.in"
+__license__ = "GNU GENERAL PUBLIC LICENSE V3"
+__copyright__ = "Copyright 2025 Saradindu Sengupta"
